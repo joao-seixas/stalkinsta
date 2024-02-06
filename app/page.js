9291436d04bc5bc324ae493a8adaf0252eaa0520
joao-seixas <joao.seixas@al.infnet.edu.br>
2024-02-06 05:32:17 -0300

    bug fix

--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
 import styles from "./page.module.css";
-import { useState } from 'react';
 
 export default function Home() {
-  const [isLogged, setIslogged] = useState(false);
-  
   return (
     <main className={styles.main}>
       <header>
